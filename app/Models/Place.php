54fb269bc958c35720cc944bb7bf9c8f633c4b2e
masangana <alexmasangana@gmail.com>
2022-12-19 20:09:00 +0300

    Add some models

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\AssignOp\Concat;
 
 class Place extends Model
 {
     use HasFactory;
+
+    public function schedules()
+    {
+        return $this->morphMany(Schedule::class, 'schedulable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function adress()
+    {
+        return $this->morphMany(Adress::class, 'adressable');
+    }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function contacts()
+    {
+        return $this->morphMany(Concat::class, 'contactable');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+
 }
--- database/migrations/2022_12_19_130739_create_services_table.php
+++ database/migrations/2022_12_19_130739_create_services_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('place_id')->constrained('places');
             $table->string('title');
             $table->text('description');
             $table->float('max_price')->unsigned();
