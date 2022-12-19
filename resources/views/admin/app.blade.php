21922742acb51ea8e91081fc10a50e89643ae006
masangana <alexmasangana@gmail.com>
2022-12-19 21:27:13 +0300

    Add main admin view parts
    - Head
    - Footer
    - Navbar
    - Sidebar
    - Script

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+    @include('admin.layouts.head')
+
+    <body>
+
+        <div class="container-scroller">
+
+            @include('admin.layouts.sidebar')
+
+            <div class="container-fluid page-body-wrapper">
+
+                @include('admin.layouts.navbar')
+
+                <div class="main-panel">
+
+                    <div class="content-wrapper">
+
+                        @yield('content')
+
+                    </div>
+
+                    @include('admin.layouts.footer')
+
+                </div>
+
+            </div>
+
+        </div>
+
+        @include('admin.layouts.scripts')
+
+    </body>
+
+</html>
