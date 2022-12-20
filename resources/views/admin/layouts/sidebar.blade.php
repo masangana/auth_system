da3541d7c64a28f8af564ed4053e4a53f4e68a3c
masangana <alexmasangana@gmail.com>
2022-12-20 12:07:45 +0300

    Fix links

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,14 +1,14 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-      <a class="sidebar-brand brand-logo" href="{{Route('admin.dashboard')}} "><img src="assets/images/logo.svg" alt="logo" /></a>
-      <a class="sidebar-brand brand-logo-mini" href="{{Route('admin.dashboard')}} "><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+      <a class="sidebar-brand brand-logo" href="{{Route('admin.dashboard')}} "><img src="{{asset ('admin/assets/images/logo.svg')}}" alt="logo" /></a>
+      <a class="sidebar-brand brand-logo-mini" href="{{Route('admin.dashboard')}} "><img src="{{asset ('admin/assets/images/logo-mini.svg')}}" alt="logo" /></a>
     </div>
     <ul class="nav">
       <li class="nav-item profile">
         <div class="profile-desc">
           <div class="profile-pic">
             <div class="count-indicator">
-              <img class="img-xs rounded-circle " src="assets/images/faces/face15.jpg" alt="">
+              <img class="img-xs rounded-circle " src="{{asset ('admin/assets/images/faces/face15.jpg')}}" alt="">
               <span class="count bg-success"></span>
             </div>
             <div class="profile-name">
@@ -32,15 +32,15 @@
       <li class="nav-item menu-items">
         <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
           <span class="menu-icon">
-            <i class="mdi mdi-laptop"></i>
+            <i class="mdi mdi-home-modern"></i>
           </span>
-          <span class="menu-title">Basic UI Elements</span>
+          <span class="menu-title">Les Places</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{ Route('place.index') }}">Tout Voir</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{ Route('place.create') }}">Ajouter</a></li>
             <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
           </ul>
         </div>
