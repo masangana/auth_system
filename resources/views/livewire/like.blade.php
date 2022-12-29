ad3140f229427299b41e6741fa6ac7fc78ddc284
masangana <alexmasangana@gmail.com>
2022-12-30 02:38:11 +0300

    first try for like

---
+++ resources/views/livewire/like.blade.php
@@ -0,0 +1,14 @@
+
+
+
+
+<div class="col-md-8 text-center text-md-left">
+    <div class="call-to-action-text">
+      <h3 class="action-title">Vous aimez ce lieux liker. Il compte déjà {{ $likes}} </h3>
+    </div>
+</div><!-- Col end -->
+<div class="col-md-4 text-center text-md-right mt-3 mt-md-0">
+    <button wire:click="like" class="btn btn-primary">
+        Like it2
+    </button>
+</div>
--- resources/views/user/place/show.blade.php
+++ resources/views/user/place/show.blade.php
@@ -173,16 +173,9 @@
   
             <div class="call-to-action classic">
               <div class="row align-items-center">
-                <div class="col-md-8 text-center text-md-left">
-                  <div class="call-to-action-text">
-                    <h3 class="action-title">Interested with this service.</h3>
-                  </div>
-                </div><!-- Col end -->
-                <div class="col-md-4 text-center text-md-right mt-3 mt-md-0">
-                  <div class="call-to-action-btn">
-                    <a class="btn btn-primary" href="#">Like it</a>
-                  </div>
-                </div><!-- col end -->
+                <!--/ Accordion end 
+                  
+                col end -->
               </div><!-- row end -->
             </div><!-- Action end -->
   
